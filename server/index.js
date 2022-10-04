27ff6d361c9a95075943bc19e9598b5e195a2e69
hha <heinhtetaung@m-techsolutions.io>
2022-10-04 15:28:56 +0630

    project setup

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const app =express()
+const {graphqlHTTP}=require('express-graphql')
+require('dotenv').config()
+const connectDB=require('./config/db')
+const schema =require('./schema/schema')
+
+connectDB()
+
+app.use('/graphql',graphqlHTTP({
+    schema,
+    graphiql:process.env.NODE_ENV==='development'
+}))
+
+app.listen(process.env.PORT,()=>{
+    console.log('server is runnion')
+})
